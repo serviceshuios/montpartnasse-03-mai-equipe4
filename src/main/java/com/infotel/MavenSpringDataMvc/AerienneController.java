d8e59cad156d1045909d8ca53d79e25fa3bda6bb
Johan VAIRAC <johan.vairac@outlook.fr>
2019-05-06 13:30:03 +0200

    commit cargaisonController

---
+++ src/main/java/com/infotel/MavenSpringDataMvc/AerienneController.java
@@ -0,0 +1,69 @@
+package com.infotel.MavenSpringDataMvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.infotel.MavenSpringDataMvc.metier.Aerienne;
+import com.infotel.MavenSpringDataMvc.metier.Cargaison;
+import com.infotel.MavenSpringDataMvc.service.Iservice;
+
+
+
+
+@Controller
+public class AerienneController {
+
+	@Autowired
+    private Iservice service;
+	
+	@RequestMapping(value = "/indexAerienne", method = RequestMethod.GET)
+    public String lister(Model model) {
+        model.addAttribute("cargaison", new Aerienne());
+        model.addAttribute("cargaisons", service.listerCargaison());
+        return "cargaisons";
+    }
+	
+	 @RequestMapping(value = "/saveAerienne")
+	    public String save(Cargaison cargaison,  Model model) {
+	        if (cargaison.getIdCargaison() == 0) {
+	           
+	        	service.ajouterCargaison(cargaison);
+	            model.addAttribute("cargaison", new Aerienne());
+	            model.addAttribute("cargaison", service.listerCargaison());
+	            return "cargaisons";
+	            
+	        } else {
+	        	
+	        	String nomCargaison = cargaison.getNomCargaison();
+	            String depart = cargaison.getDepart();
+	            String destination = cargaison.getDestination();
+	            String numVol= cargaison.getNumVol();
+	            double taxe = cargaison.getTaxe();
+	            int idCargaison= cargaison.getIdCargaison();
+	            
+	        	service.modifierAerienne (nomCargaison, depart, destination,numVol,taxe, idCargaison);
+	            model.addAttribute("cargaison", new Aerienne());
+	            model.addAttribute("cargaisons", service.listerCargaison());
+	            return "cargaisons";
+	        }
+	   }
+	 
+	 @RequestMapping(value = "/deleteAerienne")
+     public String delete(@RequestParam int idCargaison, Model model) {
+         service.supprimerCargaison(idCargaison);
+         model.addAttribute("cargaison", new Aerienne());
+         model.addAttribute("cargaisons", service.listerCargaison());
+         return "cargaison";
+     }
+	 
+	 @RequestMapping(value = "/editAerienne")
+     public String edit(@RequestParam int idCargaison, Model model) {
+         model.addAttribute("cargaison", service.getCargaison(idCargaison));
+         model.addAttribute("cargaisons", service.listerCargaison());
+         return "cargaisons";
+     }
+}
