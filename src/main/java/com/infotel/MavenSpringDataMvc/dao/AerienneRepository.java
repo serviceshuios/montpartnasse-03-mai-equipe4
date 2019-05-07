79744cfe2077667f9b22c3478eab8b881a0a5c74
Raph <raphael_bruneau@orange.fr>
2019-05-07 12:30:06 +0200

    Commit Maj des methodes lier dans repository

--- src/main/java/com/infotel/MavenSpringDataMvc/dao/AerienneRepository.java
+++ src/main/java/com/infotel/MavenSpringDataMvc/dao/AerienneRepository.java
@@ -4,13 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.infotel.MavenSpringDataMvc.metier.Aerienne;
 
+@Transactional
 public interface AerienneRepository extends JpaRepository<Aerienne, Integer>{
 
 	@Modifying
-	@Query("UPDATE Aerienne SET idSociete = :x WHERE idCargaison = :y")
+	@Query("UPDATE Aerienne SET societetransport.idSociete = :x WHERE idCargaison = :y")
 	public void lierAerienneSociete(@Param("x")int idSociete, @Param("y")int idCargaison);
 
 
--- src/main/java/com/infotel/MavenSpringDataMvc/dao/RoutiereRepository.java
+++ src/main/java/com/infotel/MavenSpringDataMvc/dao/RoutiereRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.infotel.MavenSpringDataMvc.metier.Routiere;
 
+@Transactional
 public interface RoutiereRepository extends JpaRepository<Routiere, Integer>{
 
 	@Modifying
-	@Query("UPDATE Routiere SET idSociete = :x WHERE idCargaison = :y")
+	@Query("UPDATE Routiere SET societetransport.idSociete = :x WHERE idCargaison = :y")
 	public void lierRoutiereSociete(@Param("x")int idSociete, @Param("y")int idCargaison);
 }
